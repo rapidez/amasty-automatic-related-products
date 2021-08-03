408c9649a5ecdfdcc03b0989f6d82d7508091de0
Roy Duineveld <royduin@users.noreply.github.com>
2021-08-03 11:34:46 +0000

    Apply fixes from StyleCI

--- src/AmastyAutomaticRelatedProductsServiceProvider.php
+++ src/AmastyAutomaticRelatedProductsServiceProvider.php
@@ -4,8 +4,8 @@ namespace Rapidez\AmastyAutomaticRelatedProducts;
 
 use Illuminate\Support\ServiceProvider;
 use Rapidez\AmastyAutomaticRelatedProducts\Models\Scopes\RelatedProductsScope;
-use TorMorten\Eventy\Facades\Eventy;
 use Rapidez\Core\Casts\CommaSeparatedToArray;
+use TorMorten\Eventy\Facades\Eventy;
 
 class AmastyAutomaticRelatedProductsServiceProvider extends ServiceProvider
 {
@@ -16,17 +16,17 @@ class AmastyAutomaticRelatedProductsServiceProvider extends ServiceProvider
             ->registerViews();
     }
 
-    public function addScopes() : self
+    public function addScopes(): self
     {
         Eventy::addFilter('product.scopes', fn ($scopes) => array_merge($scopes ?: [], [RelatedProductsScope::class]));
-        Eventy::addFilter('product.casts', fn($casts) => array_merge($casts ?: [], [
+        Eventy::addFilter('product.casts', fn ($casts) => array_merge($casts ?: [], [
             'amasty_related_ids' => CommaSeparatedToArray::class,
         ]));
 
         return $this;
     }
 
-    public function registerViews() : self
+    public function registerViews(): self
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'amastyrelatedproducts');
 
