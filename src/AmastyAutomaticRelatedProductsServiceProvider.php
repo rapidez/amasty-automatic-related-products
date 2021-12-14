52254bbff4437701f9d8828f9462d4c565a13082
Claudio Ferraro <claudio@justbetter.nl>
2021-12-14 10:06:24 +0100

    Added cart_into_crosssel

--- src/AmastyAutomaticRelatedProductsServiceProvider.php
+++ src/AmastyAutomaticRelatedProductsServiceProvider.php
@@ -19,6 +19,7 @@ class AmastyAutomaticRelatedProductsServiceProvider extends ServiceProvider
     public function addScopes(): self
     {
         Eventy::addFilter('product.scopes', fn ($scopes) => array_merge($scopes ?: [], [RelatedProductsScope::class]));
+        Eventy::addFilter('quote.scopes', fn ($scopes) => array_merge($scopes ?: [], [RelatedQuoteScope::class]));
         Eventy::addFilter('product.casts', fn ($casts) => array_merge($casts ?: [], [
             'amasty_related_ids' => CommaSeparatedToArray::class,
         ]));
