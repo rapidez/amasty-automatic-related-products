1f21dff8257c979b2325a2664613b346258537fa
Claudio Ferraro <54107647+jbclaudio@users.noreply.github.com>
2021-12-14 09:41:09 +0100

    Filter related products by position (#7)

--- src/Models/Scopes/RelatedProductsScope.php
+++ src/Models/Scopes/RelatedProductsScope.php
@@ -14,11 +14,13 @@ class RelatedProductsScope implements Scope
             ->leftJoin('amasty_mostviewed_product_index as mainrule', function ($join) use ($builder) {
                 $join->on('mainrule.entity_id', '=', $builder->getQuery()->from.'.entity_id')
                     ->where('mainrule.relation', 'where_show')
+                    ->whereIn('mainrule.position', ['product_into_related', 'product_into_upsell'])
                     ->where('mainrule.store_id', config('rapidez.store'));
             })
             ->leftJoin('amasty_mostviewed_product_index as related', function ($join) {
                 $join->on('related.rule_id', '=', 'mainrule.rule_id')
                     ->where('related.relation', 'what_show')
+                    ->whereIn('related.position', ['product_into_related', 'product_into_upsell'])
                     ->where('related.store_id', config('rapidez.store'));
             });
     }
